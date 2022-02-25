dfdd66690a8a64800224aada89321034bdab4a18
Deepak Pahawa <deepak.pahawa@dreamorbit.com>
2022-02-25 22:04:46 +0530

    asdfasdf

--- src/BestApproach/components/RadioButton.jsx
+++ src/BestApproach/components/RadioButton.jsx
@@ -1,17 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 export const RadioButton = ({ options, name }) => {
-    const [value, setValue] = useState('');
-
-    const onChangeHandler = useCallback(event => {
-        setValue(event?.target?.value || '');
-    }, []);
-
     return (
         <div className='flex-column'>
             {options.map((option, index) => (
                 <label key={`${option}-${index}`}>
-                    <input type='radio' name={name} value={option} onChange={onChangeHandler} />
+                    <input type='radio' name={name} value={option} onChange={() => {}} />
                     {option}
                 </label>
             ))}
